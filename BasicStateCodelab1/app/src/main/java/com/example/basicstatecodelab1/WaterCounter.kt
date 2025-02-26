c27889467ecd2b91cf45318975ca650bc6b6a0f6
Atharva <chilwerwaratharva@gmail.com>
2025-02-26 14:30:02 +0530

    Covers everything till state hoisting, still if you want to learn about state hoisting refer to its codelab

--- BasicStateCodelab1/app/src/main/java/com/example/basicstatecodelab1/WaterCounter.kt
+++ BasicStateCodelab1/app/src/main/java/com/example/basicstatecodelab1/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.basicstatecodelab1
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -10,8 +11,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,15 +34,46 @@ fun WaterCounter(modifier: Modifier = Modifier){   //with the new state, creatin
         //For example we can see that without remember the value of count will be set to 0 again
         //So to preserve the last mutated value we need to use the remember composable inline function
         if(count>0){
+            var showTask by remember { mutableStateOf(true) }
+            if(showTask){
+                WellnessTaskItem(taskName = "Have you taken your 15min walk today",
+                    onClose = {showTask = false})
+            }
         Text(
             text = "You've had $count glasses"
         )
             }
-        Button(enabled = if(count<10) true else false,  //This explains
-//            how State drives which elements are present in the UI at a given moment.
-            onClick = {count++},
-            modifier = Modifier.padding(top = 8.dp)
-        ) { Text(text = "Add one") }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                enabled = count < 10,  //This explains
+                //            how State drives which elements are present in the UI at a given moment.
+                onClick = { count++ }
+            ) { Text(text = "Add one") }
+            Button(onClick = {count = 0},
+                Modifier
+                    .padding(start = 8.dp)
+                    .weight(1f)) {
+                Text(text = "Clear water count")
+            }
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, {count++}, modifier)
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: ()-> Unit, modifier: Modifier = Modifier){
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
     }
 }
 
@@ -47,3 +82,16 @@ fun WaterCounter(modifier: Modifier = Modifier){   //with the new state, creatin
 fun WaterCounterPreview(){
     WaterCounter()
 }
+
+@Preview
+@Composable
+fun StatelessCounterPreview(){
+    StatelessCounter(count = 0, onIncrement = {})
+}
+
+@Preview
+@Composable
+fun StatefulCounterPreview(){
+    StatefulCounter()
+}
+
