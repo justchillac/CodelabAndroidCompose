5163ea9c0fe3522cba15856b26604a1b3afec513
Atharva <chilwerwaratharva@gmail.com>
2025-03-07 23:44:18 +0530

    Error test path check

--- basic-android-kotlin-compose-training-unscramble-viewmodel/app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ basic-android-kotlin-compose-training-unscramble-viewmodel/app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -5,11 +5,13 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class GameViewModelTest {
     private val viewModel = GameViewModel()
 
+    //For success path test
     @Test
     fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset(){
         var currentGameUiState = viewModel.uiState.value
@@ -26,5 +28,26 @@ class GameViewModelTest {
 
     companion object{
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+
+    //for error path test cases
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet(){
+        //Given an incorrect word as an input
+        val incorrectPlayerWord = "and"
+
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+
+        val currentGameUiState = viewModel.uiState.value
+        assertEquals(0, currentGameUiState.score)
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+    }
+
+    //For boundary cases
+    @Test
+    fun gameViewModel_Initialization_FirstWordLoaded(){
+        val gameUiState = viewModel.uiState.value
+
     }
 }
